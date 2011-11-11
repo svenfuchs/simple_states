c077f2d2ee54d6e5573c859f4cbd4c1e753b1aa7
Sven Fuchs <svenfuchs@artweb-design.de>
2011-11-11 03:21:52 +0100

    make set_state protected again. it's not accessible on the object anyway

--- lib/simple_states/event.rb
+++ lib/simple_states/event.rb
@@ -38,15 +38,6 @@ module SimpleStates
       end
     end
 
-    def set_state(object)
-      if state = target_state(object)
-        object.past_states << object.state if object.state
-        object.state = state.to_sym
-        object.send(:"#{state}_at=", Time.now) if object.respond_to?(:"#{state}_at=") && object.respond_to?(:"#{state}_at") && object.send(:"#{state}_at").nil?
-        object.save! if @saving
-      end
-    end
-
     protected
 
       def skip?(object, args)
@@ -62,6 +53,15 @@ module SimpleStates
 
       def run_callbacks(object, type, args)
         send_methods(object, options.send(type), args)
+      end
+
+      def set_state(object)
+        if state = target_state(object)
+          object.past_states << object.state if object.state
+          object.state = state.to_sym
+          object.send(:"#{state}_at=", Time.now) if object.respond_to?(:"#{state}_at=") && object.respond_to?(:"#{state}_at") && object.send(:"#{state}_at").nil?
+          object.save! if @saving
+        end
       end
 
       def target_state(object)
